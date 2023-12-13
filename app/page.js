3a77d2c8a04987507cfa5b90b57e53d02c8ae83a
Luciano Ribeiro <luciano.ar6@gmail.com>
2023-12-13 03:38:33 -0300

    style and animate home page

--- app/page.js
+++ app/page.js
@@ -1,17 +1,23 @@
 import Link from "next/link";
+import "./home.css";
+import { GiBrain } from "react-icons/gi";
+import { FaArrowRight } from "react-icons/fa";
 
 function HomePage() {
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content text-center">
         <div className="max-w-md">
-          <h1 className="text-6xl font-bold text-primary">GPTGenius</h1>
+          <h1 className="text-6xl font-bold text-primary flex justify-center">
+            <GiBrain className="mr-3 opacity-50 floating" />
+            GPTGenius
+          </h1>
           <p className="py-6 text-lg leading-loose">
             GPTGenius: Your AI language companion. Powered by OpenAI, it
             enhances your conversations, content creation, and more!
           </p>
-          <Link href="/chat" className="btn btn-primary">
-            GET STARTED
+          <Link href="/chat" className="btn btn-primary get-started">
+            GET STARTED <FaArrowRight className="btn-icon" />
           </Link>
         </div>
       </div>
