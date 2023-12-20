77bf40294571093435e71b564105b6983984ba4f
Luciano Ribeiro <luciano.ar6@gmail.com>
2023-12-20 03:44:11 -0300

    add new-tour function to retrieve tour from API/db

--- utils/action.js
+++ utils/action.js
@@ -1,5 +1,6 @@
 "use server";
 import OpenAI from "openai";
+import prisma from "./db";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
@@ -30,7 +31,14 @@ export const generateChatResponse = async (chatMessages) => {
 };
 
 export const getExistingTour = async ({ city, country }) => {
-  return null;
+  return prisma.tour.findUnique({
+    where: {
+      city_country: {
+        city,
+        country,
+      },
+    },
+  });
 };
 
 export const generateTourResponse = async ({ city, country }) => {
@@ -70,5 +78,7 @@ If you can't find info on exact ${city}, or ${city} does not exist, or it's popu
 };
 
 export const createNewTour = async ({ tour }) => {
-  return null;
+  return prisma.tour.create({
+    data: tour,
+  });
 };
