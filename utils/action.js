b8981015d79c7b4e215aa875f4a3dbc7e5080613
Luciano Ribeiro <luciano.ar6@gmail.com>
2023-12-22 04:55:11 -0300

    add tour single page

--- utils/action.js
+++ utils/action.js
@@ -107,3 +107,11 @@ export const getAllTours = async (searchTerm) => {
 
   return tours;
 };
+
+export const getSingleTour = async (id) => {
+  return prisma.tour.findUnique({
+    where: {
+      id,
+    },
+  });
+};
